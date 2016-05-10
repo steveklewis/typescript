6fe2485b1d2733e687d449c4ad2cb54770d67f3e
steveklewis <lewisksteven@gmail.com>
2016-05-09 21:57:25 -0400

    First use of a service, without async, mocked data.

--- angular2-tour-of-heroes/app/app.component.ts
+++ angular2-tour-of-heroes/app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component } from 'angular2/core';
+import { OnInit } from 'angular2/core';
 
 import { Hero } from './hero';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
 
 @Component({
     selector: 'my-app',
@@ -65,29 +67,28 @@ import { HeroDetailComponent } from './hero-detail.component';
     border-radius: 4px 0 0 4px;
   }
 `],
- directives: [HeroDetailComponent]
+ directives: [HeroDetailComponent],
+ providers: [HeroService]
 })
-export class AppComponent {
-    heroes = HEROES;
+export class AppComponent implements OnInit {
+    heroes: Hero[];
 
     title = 'Tour of Heroes';
     selectedHero: Hero;
+   
+    constructor(private heroService: HeroService) {}
+
+    ngOnInit() {
+        this.getHeroes();
+    }
     
     onSelect(hero:Hero) {
         this.selectedHero = hero;
     }
+
+    getHeroes() {
+        this.heroes = this.heroService.getHeroes();
+    }
        
 }
 
-var HEROES: Hero[] = [
-    { "id": 11, "name": "Mr. Nice" },
-    { "id": 12, "name": "Narco" },
-    { "id": 13, "name": "Bombasto" },
-    { "id": 14, "name": "Celeritas" },
-    { "id": 15, "name": "Magneta" },
-    { "id": 16, "name": "RubberMan" },
-    { "id": 17, "name": "Dynama" },
-    { "id": 18, "name": "Dr IQ" },
-    { "id": 19, "name": "Magma" },
-    { "id": 20, "name": "Tornado" }
-];
