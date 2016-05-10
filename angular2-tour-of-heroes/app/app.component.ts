43142c6e37a2d5326e486d99090a84713bd52156
steveklewis <lewisksteven@gmail.com>
2016-05-09 20:53:55 -0400

    Multiple components

--- angular2-tour-of-heroes/app/app.component.ts
+++ angular2-tour-of-heroes/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular2/core';
+import { Component } from 'angular2/core';
 
 import { Hero } from './hero';
 import { HeroDetailComponent } from './hero-detail.component';
